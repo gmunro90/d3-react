0a470c008d5ab646c4ceef032cab07bb711d30bd
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-26 17:52:43 +0200

    opening and closing success w styling

--- trading-app/src/components/Navbar.js
+++ trading-app/src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, { useState} from 'react'
 import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import { Link } from 'react-router-dom'
+import { SidebarData } from './SidebarData'
+import './Navbar.css'
+import { IconContext } from 'react-icons'
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false)
@@ -9,6 +12,7 @@ function Navbar() {
 
   return (
     <>
+    <IconContext.Provider value={{color: '#fff'}}>
       <div className="navbar">
         <Link to="#" className="menu-bars">
           <FaIcons.FaBars  onClick={showSidebar} />
@@ -16,14 +20,27 @@ function Navbar() {
       </div>
 
       <nav className={sidebar ? 'nav-menu active' : 'nav-menu' }>
-        <ul className='nav-menu-item'>
+        <ul className='nav-menu-item' onClick={showSidebar} >
           <li className="navbar-toggle">
             <Link to="#" className='menu-bars'>
               <AiIcons.AiOutlineClose />
             </Link>
           </li>
+
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })}
+
         </ul>
       </nav>
+      </IconContext.Provider>
     </>
   )
 }
