bc23b5046d6451c1f4dd12f52f5f814e3259691b
Greg Munro <gregmunro90@gmail.com>
2022-08-03 11:24:33 +0200

    axis success

--- trading-app/src/components/Linechart/LineChart.js
+++ trading-app/src/components/Linechart/LineChart.js
@@ -10,34 +10,53 @@ const svg = d3.select('body')
               .attr('height', height)
               .attr('class', 'container')
 
-              
 const LineChart = () => {
-
 const newData = []
+const newData1 = []
 
-//Y axis data
-const myData =
-  fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=compact&apikey=ZV7DIFPQUWYA1LD0')
-    .then(response => response.json())
-    .then(data => {
-      for (let key in data["Time Series (Daily)"]) {
-         newData.push(data["Time Series (Daily)"][key]["4. close"])
+async function main() {
+  const response = await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=compact&apikey=ZV7DIFPQUWYA1LD0')
+   const data = await (response.json())
+        for (let key in data["Time Series (Daily)"]) {
+        newData.push(+data["Time Series (Daily)"][key]["4. close"])
+        newData1.push(new Date(key))
+
         }
-      })
 
+console.log('parsed data', newData)
+// console.log('dates', newData1)
 
-console.log('pushed data', newData)
-
-  const yScale = d3.scaleLinear()
-            .domain([0, 160])
+const yScale = d3.scaleLinear()
+            .domain([d3.min(newData), d3.max(newData)])
             .range([height - 110, 0])
-
 
 const y_axis = d3.axisLeft(yScale);
 
 svg.append("g")
   .attr("transform", "translate(50, 50)")
   .call(y_axis)
+
+//X axis data
+const xScale = d3.scaleTime()
+                .domain([newData1[0], newData1.pop()])
+                .range([0, 600])
+
+const x_axis = d3.axisBottom(xScale)
+
+svg.append('g')
+    .attr('transform', 'translate(50, 340)')
+    .call(x_axis)
+}
+
+
+main()
+
+// svg.append('line')
+//       .attr('x1', 10)
+//       .attr('y1', 400)
+//       .attr('x2', 700)
+//       .attr('y2', 100)
+//       .attr('stroke', 'blue')
 
   // const d3Chart = useRef()
 
